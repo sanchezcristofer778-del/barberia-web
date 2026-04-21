f93f6c8f2632b08a7e67ef374ee1b8dad8e38165
sanchezcristofer778-del <sanchezcristofer778@gmail.com>
2026-04-20 21:39:23 -0500

    Actualización con servicios reales del cliente

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function irWhatsApp() {
-  window.open("https://wa.me/51926881582?text=Hola,%20quiero%20reservar%20una%20cita%20en%20la%20barbería", "_blank");
+  window.open("https://wa.me/51926881582?text=Hola,%20quiero%20reservar%20una%20cita", "_blank");
 }
