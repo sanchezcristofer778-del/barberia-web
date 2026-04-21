6bb20dbeb5671f8e3cb24f00b08ae6de305bed3c
sanchezcristofer778-del <sanchezcristofer778@gmail.com>
2026-04-21 00:12:46 -0500

    Agregando modal con imagenes

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+let servicioActual = "";
+
+function abrirModal(titulo, descripcion, precio, imagen) {
+  document.getElementById("modal").style.display = "block";
+  document.getElementById("titulo").innerText = titulo;
+  document.getElementById("descripcion").innerText = descripcion;
+  document.getElementById("precio").innerText = precio;
+  document.getElementById("imagen").src = imagen;
+
+  servicioActual = titulo;
+}
+
+function cerrarModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
 function irWhatsApp() {
-  window.open("https://wa.me/51926881582?text=Hola,%20quiero%20reservar%20una%20cita", "_blank");
+  let mensaje = "Hola, quiero reservar " + servicioActual;
+  window.open("https://wa.me/51926881582?text=" + encodeURIComponent(mensaje));
 }
